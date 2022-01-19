f172b0b0a25484d8df43bf6b12d3239f74f6159b
Safonova Alexandra <alexa-09@bk.ru>
2022-01-19 22:27:29 +0300

    feat: burger opening/closing

--- portfolio/js/hamburger.js
+++ portfolio/js/hamburger.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.header__navigation');
 
 function toggleMenu() {
   hamburger.classList.toggle('open');
+  navigation.classList.toggle('open');
 }
 hamburger.addEventListener('click', toggleMenu);
+navigation.addEventListener('click', toggleMenu);
+
+
+
+  
