0ec1c064706284e9b04173a7dc34008c029cf18f
Safonova Alexandra <alexa-09@bk.ru>
2022-01-20 17:29:58 +0300

    init: portfolio-img.js

---
+++ portfolio/js/portfolio-img.js
@@ -0,0 +1,15 @@
+const portfolioBtn = document.querySelector('.portfolio-btn');
+const portfolioBtns = document.querySelector('.portfolio__button__wrapper');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+
+
+function changeImage(event) {
+   if(event.target.classList.contains('portfolio-btn')) {
+     // здесь код функции, меняющей src изображений
+     
+   }
+ }
+portfolioBtn.addEventListener('click', () => {
+   portfolioImages.src = "./assets/img/gallery/autumn/1.jpg"
+});
+portfolioImages.forEach((img, index) => img.src = `./assets/img/gallery/winter/${index + 1}.jpg`);
