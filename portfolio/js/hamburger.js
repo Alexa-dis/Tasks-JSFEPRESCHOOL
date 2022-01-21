3e01aa80465a67186aed846f44b088c1fdbb3f5f
Safonova Alexandra <alexa-09@bk.ru>
2022-01-22 01:32:02 +0300

    fix: burger horisontal scroll

--- portfolio/js/hamburger.js
+++ portfolio/js/hamburger.js
@@ -1,12 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.header__navigation');
+const shadow = document.querySelector('.shadow');
+
 
 function toggleMenu() {
   hamburger.classList.toggle('open');
   navigation.classList.toggle('open');
+  shadow.classList.toggle('open');
+  
 }
 hamburger.addEventListener('click', toggleMenu);
 navigation.addEventListener('click', toggleMenu);
+shadow.addEventListener('click', toggleMenu);
+
 
 
 
