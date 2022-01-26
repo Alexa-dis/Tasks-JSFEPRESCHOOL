e68fab8063a20331179fa6e4eb9105420d3291a4
Safonova Alexandra <alexa-09@bk.ru>
2022-01-26 21:59:57 +0300

    fix: btn change via click in gallery

--- portfolio/js/portfolio-img.js
+++ portfolio/js/portfolio-img.js
@@ -1,9 +1,10 @@
 const portfolioBtn = document.querySelector('.portfolio-btn');
 const portfolioBtns = document.querySelector('.portfolio__button__wrapper');
 const portfolioImages = document.querySelectorAll('.portfolio-image');
-const portfolioBtnActive = document.querySelectorAll('.button_colored');
+const portfolioBtnsAll = document.querySelectorAll('.portfolio-btn');
+const portfolioBtnActive = document.querySelector('.button_colored');
 
-
+//измение картинок
 function changeImage(event) {
    if(event.target.classList.contains('portfolio-btn')) {
       if (event.target.dataset.season === 'winter') {
@@ -18,11 +19,23 @@ function changeImage(event) {
       else if (event.target.dataset.season === 'autumn') {
         portfolioImages.forEach((img, index) => img.src = `./assets/img/gallery/autumn/${index + 1}.jpg`);
       }
+      removeColoredBtn();
+      selectClicketBtn(event);
     };
+    
 };
 portfolioBtns.addEventListener('click',changeImage);
 
 
+//удаление активного тега 
+const removeColoredBtn = () => {
+  portfolioBtnsAll.forEach(portfolioBtn => {
+    portfolioBtn.classList.remove('button_colored');
+    portfolioBtn.classList.add('button_bordered');
+  })
+ };
 
-
-
+const selectClicketBtn = (event) => {
+  event.target.classList.add('button_colored');
+  event.target.classList.remove('button_bordered');
+};
