be2b55843e08ac620a9954486c9ccc8da40be472
Safonova Alexandra <alexa-09@bk.ru>
2022-02-02 23:59:56 +0300

    fix: img and audio changing

---
+++ js30/main.js
@@ -0,0 +1,80 @@
+const playBtn = document.querySelector('.btn_src');
+const btn = document.querySelector('.btn');
+const activeImg = document.querySelector('.main__wrapper');
+
+const birds = document.querySelectorAll('.header__item');
+const solovey = document.querySelector('.solovey');
+const drozd = document.querySelector('.drozd');
+const zarynka = document.querySelector('.zarynka');
+const javoronok = document.querySelector('.javoronok');
+const slavka = document.querySelector('.slavka');
+
+const audio = document.querySelector('.audio');
+const songs = ['forest', 'drozd', 'javoronok', 'slavka', 'solovey', 'zarynka']
+
+
+function playAudio() {
+    audio.currentTime = 0;
+    audio.play();
+    playBtn.src = './assets/svg/pause.svg'
+    activeImg.classList.add('play')
+}
+
+function pauseAudio() {
+    audio.pause();
+    playBtn.src = './assets/svg/play.svg'
+    activeImg.classList.remove('play')
+}
+
+playBtn.addEventListener('click', () => {
+    const isPlay = activeImg.classList.contains('play')
+    if (isPlay) {
+        pauseAudio()
+    } else {
+        playAudio()
+    }
+})
+
+//solovey audio
+function soloveyAudio() {
+    audio.src = './assets/audio/solovey.mp3'
+    document.getElementById("change").style.backgroundImage = 'url(/assets/img/solovey.jpg)'
+}
+solovey.addEventListener('click', soloveyAudio)
+
+
+//drozd audio 
+function drozdAudio() {
+    audio.src = './assets/audio/drozd.mp3'
+    document.getElementById("change").style.backgroundImage = 'url(/assets/img/drozd.jpg)'
+    //playAudio()
+}
+drozd.addEventListener('click', drozdAudio)
+
+
+//zarynka audio
+function zarynkaAudio() {
+    audio.src = './assets/audio/zarynka.mp3'
+    document.getElementById("change").style.backgroundImage = 'url(/assets/img/zarynka.jpg)'
+    //playAudio()
+}
+zarynka.addEventListener('click', zarynkaAudio)
+
+
+//javoronok audio
+function javoronokAudio() {
+    audio.src = './assets/audio/javoronok.mp3'
+    document.getElementById("change").style.backgroundImage = 'url(/assets/img/javoronok.jpg)'
+    //playAudio()
+}
+javoronok.addEventListener('click', javoronokAudio)
+
+//slavka audio
+function slavkaAudio() {
+    audio.src = './assets/audio/slavka.mp3'
+    document.getElementById("change").style.backgroundImage = 'url(/assets/img/slavka.jpg)'
+    //playAudio()
+}
+slavka.addEventListener('click', slavkaAudio)
+
+
