1151fede99f425c4c1c57b5eb9466236731dd962
Safonova Alexandra <alexa-09@bk.ru>
2022-02-12 22:34:49 +0300

    fix: background color on click change

--- random-jokes/main.js
+++ random-jokes/main.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('.joke__btn');
 const text = document.querySelector('.text');
 const author = document.querySelector('.author');
-
+const colors = ['#f57c65', '#bfda63', '#f7dab8', '#bb9bad', '#fecf72', '#7ecdd9', '#ffffff' ]
 const url = 'https://type.fit/api/quotes';
-
+const body = document.querySelector('body');
 
 async function getData() {
   const res = await fetch(url);
@@ -13,7 +13,13 @@ async function getData() {
   author.innerHTML =  data[n].author;
 }
  
+
+function changeColor() {
+  let n = Math.floor(Math.random() * colors.length);
+  body.style.backgroundColor = colors[n];
+  button.style.backgroundColor = colors[n];
+}
+
 button.addEventListener('click', getData())
 button.addEventListener('click', () => window.location.reload());
-
-
+button.addEventListener('click', changeColor())
